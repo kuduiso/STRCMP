be35b90f4cbf7f81e5aeb22c90830eb5f8e3ce65
kuduiso <ridhofauza799@gmail.com>
2020-02-07 00:04:18 +0700

    latihan git

---
+++ latihan.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>latihan</title>
+</head>
+<body>
+<h1 align="center">Bypass my Password</h1>
+<form action="" method="POST">
+	<label>Nama:</label>
+		<input type="text" name="nama">
+	<br/>
+	<label>Password:</label>
+		<input type="password" name="pass">
+	<br/>
+	<button name="submit">Log In</button>
+</form>
+
+<?php
+
+if (isset($_POST["submit"])){
+	$nama = $_POST["nama"];
+	$pass = $_POST["pass"];
+	if($nama && $pass){
+		if(strcmp(rand(), $nama) == 0){
+			echo "FLAG{W3lc0Me_R1dh0}";
+		}else{
+			echo "Try Again";
+		}
+	}else{
+		echo "Failed";
+	}
+}
+#echo rand();
+?>
+
+</body>
+</html>
+
